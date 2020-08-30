f6fa07689adb8099be896d75aa7619cfe522ba8a
David Gethers <dgethers.jr@gmail.com>
2020-08-30 00:18:55 -0400

    Add ability to set port based on environment variable

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.put("/findface", (request, response) => {
   findFace.handleApiCall(request, response, db);
 });
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
