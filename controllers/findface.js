0aacaf59f654224fcc49d6e7084b6bd0c78c4014
David Gethers <dgethers.jr@gmail.com>
2020-09-07 13:27:54 -0400

    Add ability to set environment variables via dotfiles. Setup Clarifai object in server.js

--- controllers/findface.js
+++ controllers/findface.js
@@ -1,28 +1,33 @@
 const Clarifai = require("clarifai");
 
-const app = new Clarifai.App({
-  apiKey: process.env.CLARIFAI_API_KEY,
-});
-
-const handleApiCall = (request, response) => {
-  app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, request.body.imageUrl)
-    .then((response) => response.outputs[0].data.regions)
-    .then((regions) => response.status(200).json(regions));
+const handleApiCall = (request, response, clarifaiApp) => {
+    clarifaiApp.models
+        .predict(Clarifai.FACE_DETECT_MODEL, request.body.imageUrl)
+        .then((response) => response.outputs[0].data.regions)
+        .then((regions) => response.status(200).json(regions))
+        .catch((err) => {
+            console.log('handleApiCall promise error', err)
+            response.status(400).json(err)
+        })
 };
 
-//TODO: Deprecate this
+/**
+ * @deprecated TODO: remove this
+ */
 const handleFindFace = (request, response, db) => {
-  const { email } = request.body;
-  db("users")
-    .where("email", "=", email)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => response.json(entries))
-    .catch((err) => response.status(400).json(err));
+    const {email} = request.body;
+    db("users")
+        .where("email", "=", email)
+        .increment("entries", 1)
+        .returning("entries")
+        .then((entries) => response.json(entries))
+        .catch((err) => {
+            console.log('handleFindFace promise error', err)
+            response.status(400).json(err)
+        });
 };
 
 module.exports = {
-  handleFindFace: handleFindFace,
-  handleApiCall: handleApiCall,
+    handleFindFace: handleFindFace,
+    handleApiCall: handleApiCall,
 };
--- server.js
+++ server.js
@@ -3,17 +3,26 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+const dotenv = require('dotenv');
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const findFace = require("./controllers/findface");
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+if (process.env.NODE_ENV === 'development') {
+    dotenv.config();
+}
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
-  client: "pg",
-  connection: process.env.DATABASE_URL + `?ssl=true`, 
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+});
+
+const clarifaiApp = new Clarifai.App({
+    apiKey: `${process.env.CLARIFAI_API_KEY}`,
 });
 
 const app = express();
@@ -21,22 +30,22 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post("/signin", (request, response) => {
-  signin.handleSignin(request, response, db, bcrypt);
+    signin.handleSignin(request, response, db, bcrypt);
 });
 
 app.post("/register", (request, response) => {
-  register.handleRegister(request, response, db, bcrypt);
+    register.handleRegister(request, response, db, bcrypt);
 });
 
 app.get("/profile/:userId", (request, response) => {
-  profile.handleProfile(request, response, db);
+    profile.handleProfile(request, response, db);
 });
 
 app.put("/findface", (request, response) => {
-  findFace.handleApiCall(request, response, db);
+    findFace.handleApiCall(request, response, clarifaiApp);
 });
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`app is running on port ${PORT}`);
+    console.log(`app is running on port ${PORT}`);
 });
