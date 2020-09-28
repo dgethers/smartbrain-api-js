82b890590f9f7f8d3762778706c35a3b4cf5ca13
David Gethers <dgethers.jr@gmail.com>
2020-09-28 08:38:45 -0400

    minor code clean up

--- db/migrations/20200913130740_migration_create_table.js
+++ db/migrations/20200913130740_migration_create_table.js
@@ -1,21 +1,26 @@
 exports.up = function (knex) {
-    knex.schema
-        .createTable('users', (table) => {
-            table.increments('id').primary()
-            table.string('name', 100)
-            table.text('email').unique()
-            table.bigInteger('entries').defaultTo(0)
-            table.timestamp('joined').notNullable()
+    return knex.schema.hasTable('users')
+        .then(exists => {
+            if (!exists) {
+                return knex.schema
+                    .createTable('users', (table) => {
+                        table.increments('id').primary()
+                        table.string('name', 100)
+                        table.text('email').unique()
+                        table.bigInteger('entries').defaultTo(0)
+                        table.timestamp('joined').notNullable()
+                    })
+                    .createTable('login', (table) => {
+                        table.increments('id').primary()
+                        table.string('hash', 100).notNullable()
+                        table.text('email').notNullable()
+                    });
+            }
         })
-        .createTable('login', (table) => {
-            table.increments('id').primary()
-            table.string('hash', 100).notNullable()
-            table.text('email').notNullable()
-        });
 };
 
 exports.down = function (knex) {
-    knex.schema
-        .dropTable('users')
-        .dropTable('login')
+    return knex.schema
+        .dropTableIfExists('users')
+        .dropTableIfExists('login')
 };
