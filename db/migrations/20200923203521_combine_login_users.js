48f02bd32cb25b62ce6fe9dbc1e259a4b95020ab
David Gethers <dgethers.jr@gmail.com>
2020-09-30 17:07:06 -0400

    Update migration script to copy column data from one table to another

--- db/migrations/20200923203521_combine_login_users.js
+++ db/migrations/20200923203521_combine_login_users.js
@@ -1,14 +1,20 @@
 exports.up = (knex) => {
     return knex.schema
         .hasColumn("users", "hash")
-            .then(exist => {
-                if (!exist) {
-                    return knex.schema.table("users", (table) => {
+        .then(exist => {
+            if (!exist) {
+                return knex.schema
+                    .table("users", (table) => {
                         table.string('hash', 100)
 
                     })
-                }
-        });
+                    .raw("UPDATE users\n" +
+                        "SET hash = login.hash\n" +
+                        "FROM login\n" +
+                        "WHERE users.email = login.email;");
+            }
+        })
+
 }
 
 exports.down = function (knex) {
